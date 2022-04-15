d609588b0ab884c074f660cfab7d1fc11f4de0e4
IkarusPrime <xxangamaxx@gmail.com>
2022-04-15 12:08:16 +0200

    Create project.

---
+++ PAC_06/src/main/java/com/example/PAC_06/Pac06Application.java
@@ -0,0 +1,13 @@
+package com.example.PAC_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pac06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pac06Application.class, args);
+	}
+
+}
---
+++ PAC_06/src/test/java/com/example/PAC_06/Pac06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PAC_06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pac06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
